d4d088771313998e4211f4ddc4d86c799c3c1025
MDxWARRIORxOP <mohammedsiddiqui2009@gmail.com>
2023-01-15 15:25:27 +0530

    hmm, stuff, maybe

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ interface NotFound {
 /**
  * @param animeName The name of the anime to find.
  * @returns Info about the scraped data.
+ * @see https://zoro.to
  * @example
  * ```ts
  * import { getAnimeFromZoro } from "anime-search";
@@ -66,48 +67,11 @@ async function getAnimeFromZoro(animeName: NameLike): Promise<Res | NotFound> {
   return !finalArray[0] ? { code: 404, message: "Couldn't find the specified Anime" } : finalArray[0];
 }
 
-// deprecated
-// async function getAnimeFromFreak(animeName: NameLike): Promise<Res | NotFound> {
-//   const name = typeof animeName === "function" ? animeName() : animeName;
-//   const data = await fetch(`https://animefreak.site/search?keyword=${name}`).then((res) => res.text());
-//   const $ = load(data);
-//   const array: Res[] = [];
-
-//   $("#main-content > section > div.tab-content > div > div.film_list-wrap")
-//     .find(".flw-item")
-//     .each((_index, elem) => {
-//       $(elem)
-//         .find("a")
-//         .each((_inde, element) => {
-//           let thumbnailUrl;
-
-//           for (const child of element.parent!.children) {
-//             if (child.type === "tag" && child.name === "img") {
-//               thumbnailUrl = child.attribs["data-src"];
-//             }
-//           }
-
-//           array.push({
-//             name: element.attribs["title"],
-//             url: "https://animefreak.site" + element.attribs["href"],
-//             thumbnail: thumbnailUrl,
-//             code: 200,
-//             platform: "https://animefreak.site",
-//           });
-//         });
-//     });
-
-//   const nameArray = array.map((element) => element.name);
-//   const closest = closestMatch(name, nameArray);
-//   const e = array.map((element) => (element.name === closest ? element : false));
-//   const finalArray = e.filter((el) => el);
-//   return !finalArray[0] ? { code: 404, message: "Couldn't find the specified Anime" } : finalArray[0];
-// }
-
 /**
  * @param animeName The name of the anime to find.
  * @returns Info about the scraped data.
- *  * @example
+ * @see https://zoro.to
+ *  @example
  * ```ts
  * import getAnime from "anime-search";
  *
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,47 +1,31 @@
-import getAnime, { getAnimeFromZoro} from "../src/index"
+import getAnime, { getAnimeFromZoro } from "../src/index"
 
 describe("Testing the getAnime Function.", () => {
     it("should return an object with the type \"Res\".", async () => {
         expect(await getAnime("naruto")).toHaveProperty("name")
-        expect(await getAnime("naruto")).toHaveProperty("code")
+        expect(await getAnime(() => "naruto")).toHaveProperty("code")
         expect(await getAnime("naruto")).toHaveProperty("thumbnail")
-        expect(await getAnime("naruto")).toHaveProperty("url")
+        expect(await getAnime(() => "naruto")).toHaveProperty("url")
         expect(await getAnime("naruto")).toHaveProperty("platform")
     })
 
     it("Should return an object with type \"NotFound\"", async () => {
-        expect(await getAnime("eeeeeee")).toHaveProperty("code")
+        expect(await getAnime(() => "eeeeeee")).toHaveProperty("code")
         expect(await getAnime("naeeeeeeeeeeeeeruto")).toHaveProperty("message")
     })
 })
 
 describe("Testing the getAnimeFromZoro Function.", () => {
     it("should return an object with the type \"Res\".", async () => {
-        expect(await getAnimeFromZoro("sword art online")).toHaveProperty("name")
+        expect(await getAnimeFromZoro(() => "sword art online")).toHaveProperty("name")
         expect(await getAnimeFromZoro("sword art online")).toHaveProperty("code")
-        expect(await getAnimeFromZoro("sword art online")).toHaveProperty("thumbnail")
+        expect(await getAnimeFromZoro(() => "sword art online")).toHaveProperty("thumbnail")
         expect(await getAnimeFromZoro("sword art online")).toHaveProperty("url")
-        expect(await getAnimeFromZoro("sword art online")).toHaveProperty("platform")
+        expect(await getAnimeFromZoro(() => "sword art online")).toHaveProperty("platform")
     })
 
     it("Should return an object with type \"NotFound\"", async () => {
         expect(await getAnimeFromZoro("nawedwed3deruto")).toHaveProperty("code")
-        expect(await getAnimeFromZoro("naruededededdedededto")).toHaveProperty("message")
+        expect(await getAnimeFromZoro(() => "naruededededdedededto")).toHaveProperty("message")
     })
 })
-
-// deprecated
-// describe("Testing the getAnimeFromFreak Function.", () => {
-//     it("should return an object with the type \"Res\".", async () => {
-//         expect(await getAnimeFromFreak("one piece")).toHaveProperty("name")
-//         expect(await getAnimeFromFreak("one piece")).toHaveProperty("code")
-//         expect(await getAnimeFromFreak("one piece")).toHaveProperty("thumbnail")
-//         expect(await getAnimeFromFreak("one piece")).toHaveProperty("url")
-//         expect(await getAnimeFromFreak("one piece")).toHaveProperty("platform")
-//     })
-
-//     it("Should return an object with type \"NotFound\"", async () => {
-//         expect(await getAnimeFromFreak("nawedwed3deruto")).toHaveProperty("code")
-//         expect(await getAnimeFromFreak("naruededededdedededto")).toHaveProperty("message")
-//     })
-// })
